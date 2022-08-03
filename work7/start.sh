516f98fb25d58591f0444d4cabe91167bed178e4
Dmitry <dmmd@mail.ru>
2022-08-03 16:49:42 +0300

    My commit 2022-08-03 16:49

--- work7/start.sh
+++ work7/start.sh
@@ -2,12 +2,12 @@
 
 cd /root/homework/
 git pull
-
 cd /root/homework/work7/
-
 docker-compose stop
 docker-compose rm
 docker rmi work7-prod
 docker rmi work7-dev
 
+# Homework 7:
 docker-compose up -d
+docker-compose cp dev:/root/boxfuse-sample-java-war-hello/target/hello-1.0.war /root/work7_shared_webapps/
